fc6b321737567c60c97e6201a7a3396b892ca6f2
JoitaEduard <74983329+JoitaEduard@users.noreply.github.com>
2021-12-19 15:44:01 +0200

    message1  lease enter the commit message for your changes. Lines starting

--- api/routes/alimente.js
+++ api/routes/alimente.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+//metoda de get
+//returneaza un response cu statusul 200(succes) si mesaj
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Handling GET requests to /alimente"
     });
 });
 
+//metoda de post
+//returneaza un response cu statusul 201(creare date), mesaj, elementul creat
 router.post('/', (req, res, next) => {
+    const aliment = {
+        id_aliment: req.body.id_aliment,
+        nume_aliment: req.body.nume_aliment,
+        categorie: req.body.categorie,
+        data_expirare: req.body.data_expirare,
+        id_user: req.body.id_user
+    }
     res.status(201).json({
-        message: "Handling POST requests to /alimente"
+        message: "Handling POST requests to /alimente",
+        alimentCreat: aliment
     });
 });
 
+//metoda de get pentru un anumit id
+//returneaza un response cu statusul 200(succes), mesaj si id-ul pe care l-am transmis
 router.get('/:idAliment', (req, res, next) => {
     const id = req.params.idAliment;
     if (id === 'special') {
@@ -27,12 +41,16 @@ router.get('/:idAliment', (req, res, next) => {
     }
 });
 
+//metoda de patch pentru un aliment
+//returneaza un response cu statusul 200(succes) si mesaj
 router.patch('/:idAliment', (req, res, next) => {
     res.status(200).json({
         message: 'Aliment actualizat!'
     });
 });
 
+//metoda de delete pentru un aliment
+//returneaza un response cu statusul 200(succes) si mesaj
 router.delete('/:idAliment', (req, res, next) => {
     res.status(200).json({
         message: 'Aliment sters!'
--- api/routes/rezervari.js
+++ api/routes/rezervari.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+//metoda de get
+//returneaza un response cu statusul 200(succes) si mesaj
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: "Rezervarile au fost gasite!"
     });
 });
 
+//metoda de post
+//returneaza un response cu statusul 201(creare date), mesaj, elementul creat
 router.post('/', (req, res, next) => {
+    const rezervare = {
+        id_rezervare: req.body.id_rezervare,
+        id_user: req.body.id_user
+    }
+
     res.status(201).json({
-        message: "Rezervare facuta."
+        message: "Rezervare facuta.",
+        rezervare: rezervare
     });
 });
 
+//metoda de get pentru un id
+//returneaza un response cu statusul 200(succes), mesaj, id-ul rezervarii
 router.get('/:idRezervare', (req, res, next) => {
     res.status(200).json({
         message: "Detalii rezervare",
@@ -20,6 +32,8 @@ router.get('/:idRezervare', (req, res, next) => {
     });
 });
 
+//metoda de delete
+//returneaza un response cu statusul 200(succes), mesaj, id-ul rezervarii
 router.delete('/:idRezervare', (req, res, next) => {
     res.status(200).json({
         message: "Rezervare stearsa.",
